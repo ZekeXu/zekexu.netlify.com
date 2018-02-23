e5868de1ce96e7541f5764bdfca6504476af003a
ZekeXu <zekexu18@outlook.com>
2018-02-23 08:33:15 +0800

    add link to blogs

---
+++ src/pages/blog.js
@@ -0,0 +1,43 @@
+// import React from 'react'
+// import Link from 'gatsby-link'
+
+// const SecondPage = () => (
+//   <div>
+//     <h1>Hi from the second page</h1>
+//     <p>Welcome to page 2</p>
+//     <Link to="/">Go back to the homepage</Link>
+//   </div>
+// )
+
+// export default SecondPage
+
+import React from "react";
+import PostLink from "../components/PostLink";
+
+const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+
+  return <div>{Posts}</div>;
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
--- src/pages/posts/hello-world/index.md
+++
@@ -1,7 +0,0 @@
----
-path: "/blog/hello-world"
-date: "2017-11-07"
-title: "Hello Gatsby"
----
-
-Hello world
